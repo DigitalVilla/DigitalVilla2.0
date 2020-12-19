2d515bcd7a3344ef7f7921633826818a491a4b0f
DigitalVilla <digitalvilla01@gmail.com>
2020-12-19 14:47:49 -0700

    Unregister SW

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/pages/index';
-import './app/scss/main.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app/pages/index'
+import SW from './services/serviceWorker'
+import './app/scss/main.scss'
 
-const start = String.fromCodePoint(0x2660);
-console.log(`%c${start+start} Digital Villa ${start+start} %c${'2.3'} `, `color:${'purple'}`, `color:${'Navy'}`);
+console.clear()
+const start = String.fromCodePoint(0x2660)
+console.log(
+  `%c${start + start} Digital Villa ${start + start} %c${'2.3'} `,
+  `color:${'purple'}`,
+  `color:${'Navy'}`
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
+SW.unregister()
+// SW.register();
--- src/services/serviceWorker.js
+++ src/services/serviceWorker.js
@@ -1,95 +1,115 @@
 export default (() => {
-	const activeClass = "active";
-	const hasServiceWorker = ("serviceWorker" in navigator)
-	// let isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
-	let isLoggedIn = false;
-	let isOnline = ("onLine" in navigator) && navigator.onLine;
-	let svcWorker,
-		offlineIcon,
-		swRegistration;
+  const activeClass = 'active'
+  const hasServiceWorker = 'serviceWorker' in navigator
+  // let isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+  let isLoggedIn = false
+  let isOnline = 'onLine' in navigator && navigator.onLine
+  let svcWorker, offlineIcon, swRegistration
 
-	// Offline UI elements 
-	document.addEventListener("DOMContentLoaded", () => {
-		offlineIcon = document.getElementById("online-status");
+  // Offline UI elements
+  document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+      offlineIcon = document.getElementById('online-status')
 
-		if (!isOnline) {
-			offlineIcon.classList.add(activeClass);
-		}
+      if (!isOnline) {
+        offlineIcon.classList.add(activeClass)
+      }
 
-		window.addEventListener("online", () => {
-			offlineIcon.classList.remove(activeClass);
-			isOnline = true;
-			sendStatusUpdate();
-		}, false);
+      window.addEventListener(
+        'online',
+        () => {
+          offlineIcon.classList.remove(activeClass)
+          isOnline = true
+          sendStatusUpdate()
+        },
+        false
+      )
 
-		window.addEventListener("offline", () => {
-			offlineIcon.classList.add(activeClass);
-			isOnline = false;
-			sendStatusUpdate();
-		}, false);
-	}, false);
+      window.addEventListener(
+        'offline',
+        () => {
+          offlineIcon.classList.add(activeClass)
+          isOnline = false
+          sendStatusUpdate()
+        },
+        false
+      )
+    },
+    false
+  )
 
-	// Register the service worker
-	async function register() {
-		if (hasServiceWorker) {
-			const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+  // Register the service worker
+  async function register() {
+    if (hasServiceWorker) {
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`
 
-			swRegistration = await navigator.serviceWorker.register(swUrl, {
-				updateViaCache: "none" // 
-			});
+      swRegistration = await navigator.serviceWorker.register(swUrl, {
+        updateViaCache: 'none', //
+      })
 
-			// Check if the SW is in any of these states 
-			svcWorker = swRegistration.installing || swRegistration.waiting || swRegistration.active;
-			sendStatusUpdate(svcWorker);
+      // Check if the SW is in any of these states
+      svcWorker =
+        swRegistration.installing ||
+        swRegistration.waiting ||
+        swRegistration.active
+      sendStatusUpdate(svcWorker)
 
-			// Listen fora new SW to take over
-			navigator.serviceWorker.addEventListener("controllerChange", () => {
-				svcWorker = navigator.serviceWorker.controller; // Assing the new SW 
-				sendStatusUpdate(svcWorker);
-			})
+      // Listen fora new SW to take over
+      navigator.serviceWorker.addEventListener('controllerChange', () => {
+        svcWorker = navigator.serviceWorker.controller // Assing the new SW
+        sendStatusUpdate(svcWorker)
+      })
 
-			// Init comunication
-			navigator.serviceWorker.addEventListener("message", (e) => {
-				// console.log(e);
-				const { data } = e;
-				if (data.header === "statusUpdate") { // Custom field to ask fror updates
-					// console.log("SW requested status update");
-					sendStatusUpdate(e.ports && e.ports[0]);
-				}
-				if (data.header === "newServiceWorker") {
-					console.log("new Service Worker!"); 
-					
-				}
-			}, false);
-		}
-	}
+      // Init comunication
+      navigator.serviceWorker.addEventListener(
+        'message',
+        (e) => {
+          // console.log(e);
+          const { data } = e
+          if (data.header === 'statusUpdate') {
+            // Custom field to ask for updates
+            // console.log("SW requested status update");
+            sendStatusUpdate(e.ports && e.ports[0])
+          }
+          if (data.header === 'newServiceWorker') {
+            console.log('new Service Worker!')
+          }
+        },
+        false
+      )
+    }
+  }
 
-	function sendStatusUpdate(target) {
-		sendSWMessage({ statusUpdate: { isOnline, isLoggedIn } }, target);
-	}
+  function sendStatusUpdate(target) {
+    sendSWMessage({ statusUpdate: { isOnline, isLoggedIn } }, target)
+  }
 
-	function sendSWMessage(msg, target) {
-		if (target) { // Check if there is a valid conection to transmit directly
-			target.postMessage(msg);
-		} else if (svcWorker) {
-			svcWorker.postMessage(msg);
-		} else if (navigator.serviceWorker.controller) {
-			navigator.serviceWorker.controller.postMessage(msg);
-		}
-	}
+  function sendSWMessage(msg, target) {
+    if (target) {
+      // Check if there is a valid connection to transmit directly
+      target.postMessage(msg)
+    } else if (svcWorker) {
+      svcWorker.postMessage(msg)
+    } else if (navigator.serviceWorker.controller) {
+      navigator.serviceWorker.controller.postMessage(msg)
+    }
+  }
 
-	function unregister() {
-		if (hasServiceWorker) {
-			navigator.serviceWorker.ready.then(registration => {
-				registration.unregister();
-			});
-		}
-	}
+  function unregister() {
+    if (hasServiceWorker) {
+      navigator.serviceWorker.ready
+        .then((registration) => {
+          registration.unregister()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
 
-	return {
-		unregister,
-		register
-	}
-})();
-
-
+  return {
+    unregister,
+    register,
+  }
+})()
